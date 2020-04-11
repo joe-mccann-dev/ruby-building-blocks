f6fc23d37326e526870629e3cf2c32d170c494ef
Tenacious-Qi <joe.mccann@mailbox.org>
2020-04-11 12:23:16 -0400

    fixed indentation

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -3,12 +3,12 @@ def caesar_cipher(str, key=0)
   shifted_characters = characters.map do |char|
     # map lower-case letters
     if (char.ord >= 97 && char.ord <= 122) 
-        char = ((char.ord - 97 + key) % 26 + 97).chr #translate ASCII code into text
+      char = ((char.ord - 97 + key) % 26 + 97).chr # translate ASCII code into text
     # map upper-case letters
     elsif (char.ord >= 65 && char.ord <= 90) 
-        char = ((char.ord - 65 + key) % 26 + 65).chr
+      char = ((char.ord - 65 + key) % 26 + 65).chr
     else
-      char = char #don't change punctuation or whitespace
+      char = char # don't change punctuation or whitespace
     end
   end
   shifted_characters.join

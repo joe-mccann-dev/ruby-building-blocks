17a7e8f83b15e6d13b166c064a7979bcd2e596bb
Tenacious-Qi <joe.mccann@mailbox.org>
2020-04-11 11:48:00 -0400

    initial commit

---
+++ caesar-cipher.rb
@@ -0,0 +1,19 @@
+def caesar_cipher(str, key=0)
+  characters = str.each_char.to_a
+  shifted_characters = characters.map do |char|
+    # map lower-case letters
+    if (char.ord >= 97 && char.ord <= 122) 
+        char = ((char.ord - 97 + key) % 26 + 97).chr #translate ASCII code into text
+    # map upper-case letters
+    elsif (char.ord >= 65 && char.ord <= 90) 
+        char = ((char.ord - 65 + key) % 26 + 65).chr
+    else
+      char = char #don't change punctuation or whitespace
+    end
+  end
+  shifted_characters.join
+end
+
+puts caesar_cipher("Czggj, Mpwt!", 5)
+puts caesar_cipher("What a string!", 5)
+puts caesar_cipher('Mjqqt, Btwqi!', -5)

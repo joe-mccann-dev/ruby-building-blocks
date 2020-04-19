6b6817751cff40106c43a2211ccdcb9bc7faf97b
Tenacious-Qi <joe.mccann@mailbox.org>
2020-04-19 17:46:08 -0400

    added my_count and my_map

--- enumerable-methods.rb
+++ enumerable-methods.rb
@@ -17,7 +17,7 @@ module Enumberable
     return to_enum(:my_each_with_index) unless block_given?
     n = 0
     index = 0
-    while n < self.size
+    while n < self.size 
       yield(self[n], index)         if self.is_a?(Array)
       yield(keys[n], self[keys[n]]) if self.is_a?(Hash)
       yield(to_a[n], index)         if self.is_a?(Range)
@@ -39,7 +39,7 @@ module Enumberable
     result
   end
 
-  def my_all?(pattern = nil)
+  def my_all?(pattern = nil) #not working on ranges or hashes
     result = []
     pattern_proc = Proc.new do
       my_each { |item| result << item if pattern === item } 
@@ -61,7 +61,7 @@ module Enumberable
   end
 
 
-  def my_any? (pattern = nil)
+  def my_any? (pattern = nil) #not working with ranges(only works with integer ranges) or hashes
     result = []
     pattern_proc = Proc.new do  #proc to apply if a pattern is given
       my_each { |item| result << item if pattern === item }
@@ -82,6 +82,53 @@ module Enumberable
       my_each { |item| result << item if item }
       result.empty? ? false : true # result will be empty is all values are nil or false, therefore my_any? returns false
     end
+  end
+
+  def my_none? (pattern = nil) #opposite of my_any? #not working with ranges(only works with integer ranges) or hashes
+    result = []
+    pattern_proc = Proc.new do
+      my_each { |item| result << item if pattern === item }
+      result.empty? ? true : false
+    end
+    if pattern #check first for pattern
+      if block_given? #pattern and block given
+        STDERR.puts "warning: given block not used"
+        pattern_proc.call
+      else #just a pattern given
+        pattern_proc.call
+      end
+    elsif block_given?
+      my_each { |item| result << item if yield(item) }
+      result.empty? ? true : false
+    else
+      return true if self.empty?
+      my_each { |item| result << item if item }
+      result.empty? ? true : false
+    end
+  end
+
+  def my_count(argument = nil) #fails with string ranges
+    result = []
+    count = 0
+    counter = Proc.new { |item, index| count = index + 1 }
+    if argument
+      my_each { |item| result << item if argument == item }
+      result.my_each_with_index(&counter)
+    elsif block_given?
+      my_each { |item| result << item if yield(item) }
+      result.my_each_with_index(&counter)
+    else
+      my_each_with_index(&counter)  
+    end
+    self.is_a?(Hash) ? count - 2 : count
+  end
+
+  def my_map
+    return to_enum(:my_map) unless block_given?
+    result= [] #prevent from mutating original array
+    my_each { |item| array << item } #push items of self into array that will be returned
+    my_each_with_index { |item, index| result[index] = yield(item) } #change each index of result by yielding to block
+    result
   end
     
 end 

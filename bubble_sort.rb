b77e10d9824ec67261bca8d9ee56cfb30a4b9244
Tenacious-Qi <joe.mccann@mailbox.org>
2020-06-30 15:03:30 -0400

    refactored bubble_sort

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,38 +1,35 @@
 #!/usr/bin/ruby
+
 def bubble_sort(array)
-  sorted = true 
-  i = 0
-  while (i < array.length && sorted)
-    sorted = false #if sorted = true never gets triggered below, loop stops
+  sorted = false
+  until sorted
+    sorted = true
     0.upto(array.length - 2) do |n|
       if array[n] > array[n + 1]
         array[n], array[n + 1] = array[n + 1], array[n]
-        sorted = true #continue loop
+        sorted = false
       end
     end
-    i += 1
   end
-  p array
+  array
 end
 
 def bubble_sort_by(array)
-  i = 0
-  sorted = true 
-  while (i < array.length && sorted)
-    sorted = false #if sorted = true never gets triggered below, loop stops
+  sorted = false
+  until sorted
+    sorted = true
     0.upto(array.length - 2) do |n|
       block_comparison = yield(array[n], array[n + 1])
       if block_comparison > 0
         array[n], array[n + 1] = array[n + 1], array[n]
-        sorted = true #continue loop
+        sorted = false
       end
     end
-    i += 1
   end
- p array
+  p array
 end
 
-bubble_sort([4,3,78,2,0,2])
+p bubble_sort([4,3,78,2,0,2])
 bubble_sort_by(["kitten","puppies","goats","hi","dinosaurs","dogs","cat"]) do |left,right|
     left.length - right.length
   end
